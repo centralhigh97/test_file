76d1a2408f4ebcf2d3efe4fca9b8837b55d6537c
centralhigh97 <aisha_maxwell26@yahoo.com>
2019-11-13 12:24:56 -0600

    This is a test file

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello, Lisa!!");
+
+        /* This is an expression. The data type 'int' only takes whole numbers*/
+        int myFirstNumber = (10 + 5) +(2 * 10 );
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.print(myLastOne);
 
     }
 }
